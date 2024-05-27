380c539cfade22e427638ef6c597e6bc5eb3636f
emmanuelatikese <emmanuelatikese14@gmail.com>
2024-05-27 20:22:09 +0100

    adding

--- 0x00-ES6_basic/7-getBudgetObject.js
+++ 0x00-ES6_basic/7-getBudgetObject.js
@@ -1,7 +1,9 @@
-export default function getBudgetObject(income, gdp, capita) {
-  const budget = {};
-  budget.income = income;
-  budget.gdp = gdp;
-  budget.capita = capita;
+export default function getBudgetObject(income1, gdp1, capita1) {
+  const budget = {
+    income: income1,
+    gdp: gdp1,
+    capita: capita1,
+  };
+
   return budget;
 }
