f65c16a2d3f7034da15ed15bf2eae98ea80a493b
emmanuelatikese <emmanuelatikese14@gmail.com>
2024-05-28 01:48:26 +0100

    adding

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,10 +1,13 @@
 export default function taskBlock(trueOrFalse) {
   let task = false;
   let task2 = true;
+
   if (trueOrFalse) {
-    let task = true;
-    let task2 = false;
+    task = true;
+    task2 = false;
   }
+  task = false;
+  task2 = true;
 
   return [task, task2];
 }
