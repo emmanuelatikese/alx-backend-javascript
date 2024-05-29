74293bfbef66e0ddef394b78ce3a7ac311d7d5ec
emmanuelatikese <emmanuelatikese14@gmail.com>
2024-05-29 08:32:25 +0100

    adding

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,7 +1,7 @@
 export default function handleResponseFromAPI(promise) {
-  promise.then(() => ({
+  promise.then((success) => ({
     status: 200,
-    body: 'success',
+    body: success,
   }))
     .catch(() => new Error())
     .then(() => console.log('Got a response from the API'));
