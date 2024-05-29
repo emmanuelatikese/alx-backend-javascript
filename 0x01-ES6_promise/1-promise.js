9041389d64d0f2d3ccbe386cbad34a024f4c320e
emmanuelatikese <emmanuelatikese14@gmail.com>
2024-05-29 08:16:23 +0100

    adding

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,6 +1,6 @@
-export default function getFullResponseFromAPI (success){
-    return success === true ? Promise.resolve({
-        status: 200,
-        body: 'Success'
-    }) : Promise.reject("The fake API is not working currently")
+export default function getFullResponseFromAPI(success) {
+  return success === true ? Promise.resolve({
+    status: 200,
+    body: 'Success',
+  }) : Promise.reject(new Error('The fake API is not working currently'));
 }
