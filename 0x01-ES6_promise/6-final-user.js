c414bb574b2be9359753cae18bd5712da9cea5a6
emmanuelatikese <emmanuelatikese14@gmail.com>
2024-05-29 11:40:44 +0100

    adding

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -3,9 +3,9 @@ import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
   return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
-    .then((data) => data.map((data) => [{
-      status: data.status,
-      value: data.status === 'fullfilled'
-        ? data.value : `${data.reason}`,
+    .then((data) => data.map((x) => [{
+      status: x.status,
+      value: x.status === 'fulfilled'
+        ? x.value : String(x.reason),
     }]));
 }
