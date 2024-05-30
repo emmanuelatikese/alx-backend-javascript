a213927fad2877562edf06d3cfe9627b90290de8
emmanuelatikese <emmanuelatikese14@gmail.com>
2024-05-30 20:57:19 +0100

    adding

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,7 +1,7 @@
 export default class Building {
   constructor(sqft) {
     if (this.constructor !== Building) {
-      if (typeof evacuationWarningMessage !== 'function') {
+      if (typeof this.evacuationWarningMessage !== 'function') {
         throw new Error('Class extending Building must override evacuationWarningMessage');
       }
     }
