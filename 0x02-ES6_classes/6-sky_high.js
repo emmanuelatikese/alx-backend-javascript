804db2e3d1c6b513286e8f253986e19a58f03edd
emmanuelatikese <emmanuelatikese14@gmail.com>
2024-05-30 20:53:03 +0100

    adding

---
+++ 0x02-ES6_classes/6-sky_high.js
@@ -0,0 +1,21 @@
+import Building from './5-building';
+
+export default class SkyHighBuilding extends Building {
+  constructor(sqft, floors) {
+    super(sqft);
+    this.floors = floors;
+  }
+
+  get floors() { return this._floors; }
+
+  set floors(val) {
+    if (!(typeof val === 'number')) {
+      throw new TypeError('floor must be a number');
+    }
+    this._floor = val;
+  }
+
+  evacuationWarningMessage() {
+    return `Evacuate slowly the ${this.floors} floors`;
+  }
+}
