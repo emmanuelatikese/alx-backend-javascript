3fcab9e0df0950eb1f4b43dd6950f0e95380e423
emmanuelatikese <emmanuelatikese14@gmail.com>
2024-05-30 18:18:43 +0100

    adding

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -1,28 +1,37 @@
-const HolbertonCourse = class{
-    constructor(name, length, students){
-        this.name = name;
-        this.length = length;
-        this.students = students;
+const HolbertonCourse = class {
+  constructor(name, length, students) {
+    this.name = name;
+    this.length = length;
+    this.students = students;
+  }
+
+  get name() { return this._name; }
+
+  set name(val) {
+    if (!(typeof val === 'string')) {
+      throw new TypeError('Length must be a number');
     }
-    get name(){return this._name}
-    set name (val){
-        this._name = typeof val === "string" ? val : new TypeError("Name must be String")
+    this._name = val;
+  }
+
+  get length() { return this._length; }
+
+  set length(val) {
+    if (!(typeof val === 'number')) {
+      throw new TypeError('Length must be a number');
+    }
+    this._length = val;
+  }
+
+  get students() { return this._students; }
+
+  set students(val) {
+    if (!(val.every((x) => typeof x === 'string'))) {
+      throw new TypeError('Students must be number');
     }
 
-    get length(){return this._length}
-    set length(val){
-        if ( !(typeof val === 'number')){
-            throw new TypeError("Length must be a number")
-        }
-        this._name
-    }
-
-    get students(){ return this._students}
-    set students(val){
-        if (typeof students === Array){
-            this._students = val.every(x => typeof x === "string") ? val : new TypeError("Students must have string elements")
-        }
-    }
-}
+    this._students = val;
+  }
+};
 
 export default HolbertonCourse;
