a344392f5a891d675989fc33a3f8c970d84754f6
emmanuelatikese <emmanuelatikese14@gmail.com>
2024-05-30 18:48:59 +0100

    adding

--- 0x02-ES6_classes/4-pricing.js
+++ 0x02-ES6_classes/4-pricing.js
@@ -1,8 +1,7 @@
 import Currency from './3-currency';
 
-export default class Pricing extends Currency {
+export default class Pricing {
   constructor(amount, currency) {
-    super();
     this.amount = amount;
     this.currency = currency;
   }
@@ -14,6 +13,15 @@ export default class Pricing extends Currency {
       throw new TypeError('Amount must be a number');
     }
     this._amount = val;
+  }
+
+  get currency() { return this._currency; }
+
+  set currency(val) {
+    if (!(val instanceof Currency)) {
+      throw new TypeError('Must be an instance of Currency');
+    }
+    this._currency = val;
   }
 
   displayFullPrice() { return `${this._amount} ${this.currency.name} ${this.currency.code}`; }
