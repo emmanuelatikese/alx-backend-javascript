b92ea2920dee7a41a757a03d56325672bc6540f6
emmanuelatikese <emmanuelatikese14@gmail.com>
2024-05-30 21:28:36 +0100

    adding

---
+++ 0x02-ES6_classes/7-airport.js
@@ -0,0 +1,20 @@
+export default class Airport {
+  constructor(name, code) {
+    this.name = name;
+    this.code = code;
+  }
+
+  get name() { return this._name; }
+
+  set name(val) {
+    this._name = val;
+  }
+
+  get code() { return this._name; }
+
+  set code(val) { this._code = val; }
+
+  get [Symbol.toStringTag]() {
+    return this._code;
+  }
+}
