f8a970eb13d35b98e9ee6a6238197eb6d9c064f6
emmanuelatikese <emmanuelatikese14@gmail.com>
2024-05-30 23:29:13 +0100

    adding

---
+++ 0x02-ES6_classes/10-car.js
@@ -0,0 +1,25 @@
+export default class Car {
+  constructor(brand, motor, color) {
+    this.brand = brand;
+    this.motor = motor;
+    this.color = color;
+  }
+
+  get brand() { return this._brand; }
+
+  set brand(val) { this._brand = val; }
+
+  get motor() { return this._motor; }
+
+  set motor(val) { this._motor = val; }
+
+  get color() { return this._color; }
+
+  set color(val) { this._color = val; }
+
+  static get [Symbol.species]() { return this; }
+
+  cloneCar() {
+    return new this.constructor[Symbol.species]();
+  }
+}
