835409b44231a718501992f2b8cd0691f92d1590
emmanuelatikese <emmanuelatikese14@gmail.com>
2024-06-05 01:29:47 +0100

    adding all files

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,8 +1,8 @@
-const cleanSet = (set, startString) => {
+export default function cleanSet(set, startString){
   const newArr = [];
   set.forEach((x) => newArr.push(x));
-  return startString !== ''
+  return newArr.some((x) => x.startsWith(startString)) && startString !== ''
     ? newArr.filter((x) => x.startsWith(startString)).map((y) => y.slice(startString.length)).join('-')
     : '';
 };
-export default cleanSet;
+
