32d4324cf00be9db4d279b5b0f232869682490e6
emmanuelatikese <emmanuelatikese14@gmail.com>
2024-06-05 01:21:46 +0100

    adding all files

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -2,6 +2,6 @@ const weakMap = new WeakMap();
 const queryAPI = (endpoint) => {
   const t = weakMap.get(endpoint) || 0;
   weakMap.set(endpoint, t + 1);
-  if (t >= 5) throw new Error('Endpoint load is high');
+  if (t >= 4) throw new Error('Endpoint load is high');
 };
 export { weakMap, queryAPI };
