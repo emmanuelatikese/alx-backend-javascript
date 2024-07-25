51ac18fde4c2728e786aaee7fd9141fcfc96f672
emmanuelatikese <emmanuelatikese14@gmail.com>
2024-07-25 22:26:56 +0100

    adding all files

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -1,28 +1,20 @@
 const assert = require('assert');
 const calculateNumber = require('./0-calcul');
 
-describe('it should add two numbers', () => {
+describe('should add two numbers', () => {
   it('should add two integers', () => {
-    const a = 1;
-    const b = 3;
-    assert.strictEqual(calculateNumber(a, b), 4);
+    assert.strictEqual(calculateNumber(1, 3), 4);
   });
 
   it('should round and add a whole number and a decimal', () => {
-    const a = 1;
-    const b = 3.7;
-    assert.strictEqual(calculateNumber(a, b), 5);
+    assert.strictEqual(calculateNumber(3.7, 1), 5);
   });
 
   it('should round and add two decimals', () => {
-    const a = 1.2;
-    const b = 3.7;
-    assert.strictEqual(calculateNumber(a, b), 5);
+    assert.strictEqual(calculateNumber(3.7, 1.2), 5);
   });
 
   it('should round up and add two decimals', () => {
-    const a = 1.5;
-    const b = 3.7;
-    assert.strictEqual(calculateNumber(a, b), 6);
+    assert.strictEqual(calculateNumber(1.5, 3.7), 6);
   });
 });
