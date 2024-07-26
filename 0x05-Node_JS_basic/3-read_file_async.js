2271a9b4311c6391a6a0502fe6fa3b73261c1b12
emmanuelatikese <emmanuelatikese14@gmail.com>
2024-07-26 22:21:13 +0100

    updating

--- 0x05-Node_JS_basic/3-read_file_async.js
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -1,25 +1,27 @@
-const fs = require('fs');
+const fs = require('fs').promises;
 
-const countStudents = (path) => {
-  try {
-    const student = fs.readFile(path, 'utf8');
-
-    const newStudent = student.trim().split('\n');
+const countStudents = (path) => fs.readFile(path, 'utf8')
+  .then((data) => {
+    const newStudent = data.trim().split('\n');
     const header = newStudent[0].trim().split(',');
     const rows = newStudent.slice(1).map((r) => r.trim().split(','));
     const jsonData = rows.map((row) => header.reduce((acc, key, index) => {
       acc[key] = row[index];
       return acc;
     }, {}));
+
     console.log(`Number of students: ${jsonData.length}`);
+
     const numCs = jsonData.filter((x) => x.field === 'CS');
     const numSWE = jsonData.filter((x) => x.field === 'SWE');
 
-    console.log(`Number of students in FIELD: ${numCs.length}. List: ${numCs.map((x) => x.firstname).join(', ')}`);
-    console.log(`Number of students in FIELD: ${numSWE.length}. List: ${numSWE.map((x) => x.firstname).join(', ')}`);
-  } catch (err) {
+    console.log(`Number of students in CS: ${numCs.length}. List: ${numCs.map((x) => x.firstname).join(', ')}`);
+    console.log(`Number of students in SWE: ${numSWE.length}. List: ${numSWE.map((x) => x.firstname).join(', ')}`);
+
+    return true;
+  })
+  .catch(() => {
     throw new Error('Cannot load the database');
-  }
-};
+  });
 
 module.exports = countStudents;
