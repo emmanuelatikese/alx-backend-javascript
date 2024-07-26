c505c59a5191d82b4c5764a7c3624e8a1c934b1d
emmanuelatikese <emmanuelatikese14@gmail.com>
2024-07-26 23:21:22 +0100

    updating

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -1,7 +1,7 @@
 const http = require('http');
 
 const port = 1245;
-const app = app.createServer((req, res) => {
+const app = http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
   res.end('Hello Holberton School!');
 });
