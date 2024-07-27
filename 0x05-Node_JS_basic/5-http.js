586c1558cbe744b672f3692f68a181cf8fbdcb13
emmanuelatikese <emmanuelatikese14@gmail.com>
2024-07-27 23:17:35 +0100

    updating

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -1,7 +1,7 @@
 const http = require('http');
 const countStudents = require('./3-read_file_async');
 
-const db = process.argv[2] || 'database.csv';
+const db = process.argv[2];
 const port = 1245;
 const app = http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
@@ -11,7 +11,13 @@ const app = http.createServer((req, res) => {
       res.write('This is the list of our students\n');
       x.forEach((msg) => res.write(`${msg}`));
       res.end();
+    }).catch((err) => {
+      res.write(err.message);
+      res.end();
     });
+  } else {
+    res.writeHead(404);
+    res.end('Not Found');
   }
 });
 app.listen(port, () => console.log(`listening on port: ${port}`));
