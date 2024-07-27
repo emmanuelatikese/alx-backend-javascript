067fb392572106746dc9b0273b09aab605aa0353
emmanuelatikese <emmanuelatikese14@gmail.com>
2024-07-27 15:42:50 +0100

    updating

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -1,14 +1,14 @@
 const http = require('http');
 const countStudents = require('./3-read_file_async');
-
+const db = process.argv[2] || 'database.csv';
 const port = 1245;
 const app = http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
   if (req.url === '/') { res.end('Hello Holberton School!'); }
   if (req.url === '/students') {
-    countStudents('database.csv').then((x) => {
+    countStudents(db).then((x) => {
       res.write('This is the list of our students\n');
-      x.forEach((msg) => res.write(`${msg}\n`));
+      x.forEach((msg) => res.write(`\n${msg}`));
       res.end();
     });
   }
