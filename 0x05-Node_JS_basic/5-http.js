501d1f151384dd026e1761de8a3692ffc5e49393
emmanuelatikese <emmanuelatikese14@gmail.com>
2024-07-27 15:27:42 +0100

    updating

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -1,15 +1,17 @@
 const http = require('http');
 const countStudents = require('./3-read_file_async');
+
 const port = 1245;
 const app = http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
-  if (req.url === '/') {res.end('Hello Holberton School!');}
-  if (req.url === '/students'){
-    countStudents('database.csv').then(x => {
+  if (req.url === '/') { res.end('Hello Holberton School!'); }
+  if (req.url === '/students') {
+    countStudents('database.csv').then((x) => {
       res.write('This is the list of our students\n');
-      x.forEach(msg => res.write(msg + '\n'));
+      x.forEach((msg) => res.write(`${msg}\n`));
       res.end();
-    })
-}});
+    });
+  }
+});
 app.listen(port, () => console.log(`listening on port: ${port}`));
 module.exports = app;
